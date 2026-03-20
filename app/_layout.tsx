48ac665cec014f5328ea74f812cc211ed138a18e
Alex Bhusal <alexbhusal@icloud.com>
2026-03-20 17:31:33 +0545

    feat: added search screen in _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
     <>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="searchScreen" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </>
